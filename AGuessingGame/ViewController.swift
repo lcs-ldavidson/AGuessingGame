3eaee52b335aa41638558555f0a81d77bb39a8af
Liam Davidson <ldavidson@lcs.on.ca>
2019-10-14 14:47:16 -0400

    Made start-up talking work

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
 
+    
+    //MARK: Initializers
+    
+    
+    //MARK: Methods (functions) - behaviours
+    
+    //Runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //make an object named synthesizer which is an instance of the class AVSynthesizer
+        let synthesizer = AVSpeechSynthesizer()
+        
+        //make a string that contains what we want the computer to say
+        let message = "I'm thinking of a number between 1 and 100. Guess what it is."
+        
+        //make an object named utterance, which is an instance of the class AVSpeechUtterance
+        let utterance = AVSpeechUtterance(string: message)
+        
+        //say the message
+        synthesizer.speak(utterance)
+        
     }
 
 
